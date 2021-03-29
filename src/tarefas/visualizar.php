28676453a6328e8f577737f7a2d3058b88b71a8d
thegiovanesilva <48033682+thegiovanesilva@users.noreply.github.com>
2021-03-29 16:30:39 -0300

    Add files via upload
    Adição da tela de visualização de tarefas

---
+++ src/tarefas/visualizar.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="visualizar.css">
+
+<?php include("../includes/header.php") ?>
+    <main>
+        <div class="screen">
+        <h1 id="titulo">Tarefas</h1>
+            <ol>
+                <li>Tarefa X</li>
+            <ol>
+        </div>
+    </main>
+<?php include("../includes/footer.php") ?>
