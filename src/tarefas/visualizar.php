94ee11d8366dbc4d0a548a339e49d4651a3dba49
thegiovanesilva <48033682+thegiovanesilva@users.noreply.github.com>
2021-03-29 17:27:27 -0300

    Add files via upload
    Adição de áreas de visualização para tarefas

--- src/tarefas/visualizar.php
+++ src/tarefas/visualizar.php
@@ -3,10 +3,69 @@
 <?php include("../includes/header.php") ?>
     <main>
         <div class="screen">
-        <h1 id="titulo">Tarefas</h1>
-            <ol>
-                <li>Tarefa X</li>
-            <ol>
-        </div>
+        <div class="tarefas-do-dia">
+            <h1 id="titulo">Tarefas do dia</h1>
+                <ol>
+                    <li>Tarefa X
+                    <table class="tarefa">
+                        <tr>
+                            <td>Nome</td>
+                            <td>Tarefa</td>
+                        </tr>
+                        <tr>
+                            <td>Descrição</td>
+                            <td>Lacus nulla suspendisse metus, morbi</td>
+                        </tr>
+                        <tr>
+                            <td>Data limite: 00/00/00</td>
+                            <td>Data finalização: 00/00/00</td>
+                            <td>Data recorrente: Domingo<td>
+                        </tr>
+                    </table>
+                    </li>
+                    <li>Tarefa Z
+                    <table>
+                        <tr>
+                            <td>Nome</td>
+                            <td>Tarefa</td>
+                        </tr>
+                        <tr>
+                            <td>Descrição</td>
+                            <td>Lacus nulla suspendisse metus, morbi</td>
+                        </tr>
+                        <tr>
+                            <td>Data limite: 00/00/00</td>
+                            <td>Data finalização: 00/00/00</td>
+                            
+                        </tr>
+                    </table>
+                    </li>
+
+                <ol>
+            </div>
+            <div class="tarefas-gerais">
+            <h1 id="titulo">Tarefas gerais</h1>
+                <ol>
+                    <li>Tarefa Y
+                    <table>
+                        <tr>
+                            <td>Nome</td>
+                            <td>Tarefa</td>
+                        </tr>
+                        <tr>
+                            <td>Descrição</td>
+                            <td>Lacus nulla suspendisse metus, morbi</td>
+                        </tr>
+                        <tr>
+                            <td>Data limite: 00/00/00</td>
+                            <td>Data finalização: 00/00/00</td>
+                            <td>Data recorrente: Domingo<td>
+                        </tr>
+                    </table>
+                    </li>
+
+                <ol>
+            </div>
+</div>
     </main>
 <?php include("../includes/footer.php") ?>
