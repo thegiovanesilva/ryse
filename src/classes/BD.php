1242817d46f9836b8d2a81f3b47d2df60859fb25
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-03-29 16:29:51 -0300

    História 8

---
+++ src/classes/BD.php
@@ -0,0 +1,66 @@
+<?php
+class BD {
+    private $host = "localhost";
+    private $user = "admryse";
+    private $password = "12345";
+    private $database = "ryse";
+    private $conexao;
+
+    function __construct() {
+        $this->conexao = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+        mysqli_query($this->conexao, "set names 'utf8'");
+    }
+
+    function query($sql) {
+        return mysqli_query($this->conexao, $sql);
+    }
+
+    function select($sql) {
+        $retorno = mysqli_query($this->conexao, $sql);
+        $arrayResultados = array();   
+        if (mysqli_num_rows($retorno) > 0) {
+            while($linha = mysqli_fetch_assoc($retorno)) {
+                $arrayResultados[] = $linha;
+            }
+        }
+        return $arrayResultados;
+    }
+
+    function execute($stmt, $get_id=0) {
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $id = $this->conexao->insert_id;
+        $ar = $stmt->affected_rows;
+
+        if ($ar==-1) {
+            print_r($stmt->error);
+        }
+
+        $stmt->close();
+
+        if (is_bool($result)) {
+            if (!$get_id || $ar==-1) return $ar;
+            return $id;
+        }
+
+        $arrayResultados = array();
+        while ($row = $result->fetch_assoc()) {
+            $arrayResultados[] = $row;
+        }
+
+        return $arrayResultados;
+    }
+
+    function prepare($sql) {
+        return $this->conexao->prepare($sql);
+    }
+
+    function erro() {
+        return mysqli_error($this->conexao);
+    }
+
+    function info() {
+        return var_dump($this->conexao->info);
+    }
+}
+?>
---
+++ src/classes/Tarefa.php
@@ -0,0 +1,91 @@
+<?php
+require_once ("BD.php");
+class Tarefa {
+    private $conexao;
+
+    function __construct() {
+        $this->conexao = new BD();
+    }
+
+    function novaTarefa($nome, $descricao, $data_limite, $repeticao) {
+        //Tarefa com prazo recorente
+        if (count($repeticao)) {
+            $this->conexao->query("begin");
+            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao) VALUES (?, ?)");
+            $stmt->bind_param("ss",$nome,$descricao);
+            $result = $this->conexao->execute($stmt, 1);
+            if ($result==-1) {
+                $this->conexao->query("rollback");
+                return "Falha no cadastro";
+            }
+            foreach ($repeticao as $rep) {
+                $stmt = $this->conexao->prepare("INSERT INTO tarefa_reps (tarefa_id,dia) VALUES (?,?)");
+                $stmt->bind_param("is",$result,$rep);
+                $result1 = $this->conexao->execute($stmt);
+                if ($result1==-1) {
+                    $this->conexao->query("rollback");
+                    return "Falha no cadastro";
+                }
+            }
+            $this->conexao->query("commit");
+            return "Tarefa criada com sucesso!";
+        }
+
+        //Tarefa com prazo único
+        else {
+            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao, data_limite) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss",$nome,$descricao,$data_limite);
+            $result = $this->conexao->execute($stmt);
+            return $result!=-1 ? "Tarefa criada com sucesso" : "Falha no cadastro";
+        }
+    }
+
+    function apagarTarefa($id) {
+        $stmt = $this->conexao->prepare("DELETE FROM tarefa_reps WHERE tarefa_id=?");
+        $stmt->bind_param("i",$id);
+        $result = $this->conexao->execute($stmt);
+        if ($result==-1) return "Falha ao apagar";
+        $stmt = $this->conexao->prepare("DELETE FROM tarefas WHERE id=?;");
+        $stmt->bind_param("i",$id);
+        $result = $this->conexao->execute($stmt);
+        if ($result==-1) return "Falha ao apagar";
+        return "Tarefa apagada com sucesso!";
+    }
+
+    function atualizarTarefa($id, $nome, $descricao, $data_limite, $data_fim, $repeticao) {
+        $this->conexao->query("begin");
+        //atualiza tarefa simples
+        $stmt = $this->conexao->prepare("UPDATE tarefas SET nome=?,descricao=?,data_limite=?,data_fim=? WHERE id=?");
+        $stmt->bind_param("ssssi", $nome, $descricao, $data_limite, $data_fim, $id);
+        $result = $this->conexao->execute($stmt);
+        if ($result==-1) {
+            return "Falha ao atualizar";
+            $this->conexao->query("rollback");
+        }
+        //remove antigas repetições
+        $stmt = $this->conexao->prepare("DELETE FROM tarefa_reps WHERE tarefa_id=?");
+        $stmt->bind_param("i", $id);
+        $result = $this->conexao->execute($stmt);
+        if ($result==-1) {
+            return "Falha ao atualizar";
+            $this->conexao->query("rollback");
+        }
+        //cadastra novas repetições
+        foreach ($repeticao as $rep) {
+            $stmt = $this->conexao->prepare("INSERT INTO tarefa_reps (tarefa_id,dia) VALUES (?,?)");
+            $stmt->bind_param("is",$id,$rep);
+            $result = $this->conexao->execute($stmt);
+            if ($result==-1) {
+                $this->conexao->query("rollback");
+                return "Falha na atualização";
+            }
+        }
+        $this->conexao->query("commit");
+        return "Tarefa atualizada com sucesso!";
+    }
+}
+
+//$tar = new Tarefa();
+//print($tar->novaTarefa("Tarefa teste", "Teste teste teste", date("2021-03-29"), ['seg']));
+//print($tar->apagarTarefa(7));
+//print($tar->atualizarTarefa(7, "Tarefa teste 2", "Teste teste teste", NULL, date("2021-03-29"), ['ter']));
