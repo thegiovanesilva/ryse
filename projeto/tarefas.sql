04f77fa4609b0dbd69f6c10ce1629ae8cc160172
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-04-26 15:35:25 -0300

    Tarefa 15

--- projeto/tarefas.sql
+++ projeto/tarefas.sql
@@ -1,12 +1,12 @@
 --Inserir tarefa com prazo único
-INSERT INTO tarefas (nome, descricao, data_limite) VALUES ('Tarefa x', 'Fazer x e y', '2021-3-29');
+INSERT INTO tarefas (nome, descricao, data_limite, intervalos_estimados) VALUES ('Tarefa x', 'Fazer x e y', '2021-3-29', 2);
 
 --Inserir tarefa com prazo recorrente
-INSERT INTO tarefas (nome, descricao) VALUES ('Tarefa y', 'Fazer z');
+INSERT INTO tarefas (nome, descricao, intervalos_estimados) VALUES ('Tarefa y', 'Fazer z', 2);
 INSERT INTO tarefa_reps (tarefa_id, dia) VALUES (0,'seg');
 
 --Atualizar tarefa
-UPDATE tarefas SET nome='Tarefa não x',descricao='Fazer x e y',data_limite='2021-3-29',data_fim=NULL WHERE id=0;
+UPDATE tarefas SET nome='Tarefa não x',descricao='Fazer x e y',data_limite='2021-3-29',data_fim=NULL,intervalos_estimados=3 WHERE id=0;
 
 --Atualizar prazo recorrente
 DELETE FROM tarefa_reps WHERE tarefa_id=0;
@@ -14,10 +14,16 @@ INSERT INTO tarefa_reps (tarefa_id, dia) VALUES (0,'ter');
 
 --Remover tarefa
 DELETE FROM tarefa_reps WHERE tarefa_id=0;
+DELETE FROM intervalos WHERE tarefa_id=0;
 DELETE FROM tarefas WHERE id=0;
 
 --Listar tarefas
-SELECT * FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id;
+SELECT t.*, tr.*, i.dia AS intervalo_dia, i.quantidade AS intervalo_quantidade FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id LEFT JOIN intervalos i ON i.tarefa_id=t.id;
 
 --Listar uma tarefa
-SELECT * FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id WHERE t.id=0;
+SELECT t.*, tr.*, i.dia AS intervalo_dia, i.quantidade AS intervalo_quantidade FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id LEFT JOIN intervalos i ON i.tarefa_id=t.id WHERE t.id=0;
+
+--Inserir/atualizar intervalo
+SELECT * FROM intervalos WHERE tarefa_id=0 AND dia='2021-4-26';
+INSERT INTO intervalos (tarefa_id,dia,quantidade) VALUES (0, '2021-4-26', 1);
+UPDATE intervalos SET quantidade=0 WHERE tarefa_id=0 AND dia='2021-4-26';
--- src/classes/Tarefa.php
+++ src/classes/Tarefa.php
@@ -7,12 +7,12 @@ class Tarefa {
         $this->conexao = new BD();
     }
 
-    function novaTarefa($nome, $descricao, $data_limite, $repeticao) {
+    function novaTarefa($nome, $descricao, $data_limite, $repeticao, $intervalos_estimados=0) {
         //Tarefa com prazo recorente
         if (count($repeticao)) {
             $this->conexao->query("begin");
-            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao) VALUES (?, ?)");
-            $stmt->bind_param("ss",$nome,$descricao);
+            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao, intervalos_estimados) VALUES (?, ?, ?)");
+            $stmt->bind_param("ssi",$nome,$descricao,$intervalos_estimados);
             $result = $this->conexao->execute($stmt, 1);
             if ($result==-1) {
                 $this->conexao->query("rollback");
@@ -33,8 +33,8 @@ class Tarefa {
 
         //Tarefa com prazo único
         else {
-            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao, data_limite) VALUES (?, ?, ?)");
-            $stmt->bind_param("sss",$nome,$descricao,$data_limite);
+            $stmt = $this->conexao->prepare("INSERT INTO tarefas (nome, descricao, data_limite, intervalos_estimados) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("sssi",$nome,$descricao,$data_limite,$intervalos_estimados);
             $result = $this->conexao->execute($stmt);
             return $result!=-1 ? "Tarefa criada com sucesso" : "Falha no cadastro";
         }
@@ -45,6 +45,10 @@ class Tarefa {
         $stmt->bind_param("i",$id);
         $result = $this->conexao->execute($stmt);
         if ($result==-1) return "Falha ao apagar";
+        $stmt = $this->conexao->prepare("DELETE FROM intervalos WHERE tarefa_id=?");
+        $stmt->bind_param("i",$id);
+        $result = $this->conexao->execute($stmt);
+        if ($result==-1) return "Falha ao apagar";
         $stmt = $this->conexao->prepare("DELETE FROM tarefas WHERE id=?;");
         $stmt->bind_param("i",$id);
         $result = $this->conexao->execute($stmt);
@@ -52,11 +56,11 @@ class Tarefa {
         return "Tarefa apagada com sucesso!";
     }
 
-    function atualizarTarefa($id, $nome, $descricao, $data_limite, $data_fim, $repeticao) {
+    function atualizarTarefa($id, $nome, $descricao, $data_limite, $data_fim, $repeticao, $intervalos_estimados=0) {
         $this->conexao->query("begin");
         //atualiza tarefa simples
-        $stmt = $this->conexao->prepare("UPDATE tarefas SET nome=?,descricao=?,data_limite=?,data_fim=? WHERE id=?");
-        $stmt->bind_param(" ", $nome, $descricao, $data_limite, $data_fim, $id);
+        $stmt = $this->conexao->prepare("UPDATE tarefas SET nome=?,descricao=?,data_limite=?,data_fim=?,intervalos_estimados=? WHERE id=?");
+        $stmt->bind_param("ssssii", $nome, $descricao, $data_limite, $data_fim, $intervalos_estimados, $id);
         $result = $this->conexao->execute($stmt);
         if ($result==-1) {
             return "Falha ao atualizar";
@@ -86,10 +90,10 @@ class Tarefa {
 
     function buscarTarefas($id=NULL) {
         if ($id==NULL) {
-            $ret = $this->conexao->select("SELECT * FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id;");
+            $ret = $this->conexao->select("SELECT t.*, tr.*, i.dia AS intervalo_dia, i.quantidade AS intervalo_quantidade FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id LEFT JOIN intervalos i ON i.tarefa_id=t.id;");
         }
         else {
-            $stmt = $this->conexao->prepare("SELECT * FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id WHERE t.id=?;");
+            $stmt = $this->conexao->prepare("SELECT t.*, tr.*, i.dia AS intervalo_dia, i.quantidade AS intervalo_quantidade FROM tarefas t LEFT JOIN tarefa_reps tr ON tr.tarefa_id=t.id LEFT JOIN intervalos i ON i.tarefa_id=t.id WHERE t.id=?;");
             $stmt->bind_param("i",$id);
             $ret = $this->conexao->execute($stmt);
             if ($ret==-1) {
@@ -108,6 +112,11 @@ class Tarefa {
                     $result[$r["id"]]["repete"]=array();
                     $result[$r["id"]]["repete"][]=$r["dia"];
                 }
+
+                $ret2 = $this->conexao->select("SELECT * FROM intervalos WHERE tarefa_id=".$r["id"]." AND dia='".date("Y-m-d")."';");
+                if (!empty($ret2)) {
+                    $result[$r["id"]]["intervalos"] = $ret2[0]["quantidade"];
+                }
             }
             else {
                 $result[$r["id"]]["repete"][]=$r["dia"];
@@ -115,11 +124,36 @@ class Tarefa {
         }
         return $result;
     }
+
+    function cadastraIntervalo($tarefa, $dia) {
+        $stmt = $this->conexao->prepare("SELECT * FROM intervalos WHERE tarefa_id=? AND dia=?;");
+        $stmt->bind_param("is", $tarefa, $dia);
+        $result = $this->conexao->execute($stmt);
+        if (empty($result)) {
+            $stmt = $this->conexao->prepare("INSERT INTO intervalos (tarefa_id,dia,quantidade) VALUES (?, ?, ?);");
+            $t = 1;
+            $stmt->bind_param("isi", $tarefa, $dia, $t);
+            $result = $this->conexao->execute($stmt);
+            if ($result!=-1) {
+                return "Ok";
+            }
+            return "Falha";
+        }
+        $stmt = $this->conexao->prepare("UPDATE intervalos SET quantidade=? WHERE tarefa_id=? AND dia=?;");
+        $t = $result[0]["quantidade"]+1;
+        $stmt->bind_param("iis", $t, $tarefa, $dia);
+        $result = $this->conexao->execute($stmt);
+        if ($result>0) {
+            return "Ok";
+        }
+        return "Falha";
+    }
 }
 
 //$tar = new Tarefa();
-//print($tar->novaTarefa("Tarefa teste", "Teste teste teste", date("2021-03-29"), ['seg']));
-//print($tar->apagarTarefa(7));
-//print($tar->atualizarTarefa(7, "Tarefa teste 2", "Teste teste teste", NULL, date("2021-03-29"), ['ter','qua']));
+//print($tar->novaTarefa("Tarefa teste", "Teste teste teste", date("2021-03-29"), ['seg'], 1));
+//print($tar->apagarTarefa(1));
+//print($tar->atualizarTarefa(1, "Tarefa teste 2", "Teste teste teste", NULL, date("2021-03-29"), ['ter','qua'], 3));
 //print_r($tar->buscarTarefas());
 //print_r($tar->buscarTarefas(7));
+//print_r($tar->cadastraIntervalo(1, date('2021-04-26')));
